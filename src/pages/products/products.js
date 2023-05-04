afc70c360e324adfb3ca905d90b5ebac0b157fd8
SeangLeng <seangleng507@gmail.com>
2023-05-05 02:11:18 +0700

    done

--- src/pages/products/products.js
+++ src/pages/products/products.js
@@ -1,90 +1,97 @@
 const { useState, useEffect } = require("react");
 const { default: Layout } = require("../../../components/layout");
-import DataTable from 'react-data-table-component';
+import DataTable from 'react-data-table-component'
 
-export default function Products() {
-    const api_link = 'https://api.escuelajs.co/api/v1/products'
-    const [product, setproduct] = useState([])
-    fetch(api_link).then(res => res.json()).then(data => setproduct(data))
+function Products({ product }) {
+    const [data, setData] = useState(product)
+    const [search, setSearch] = useState(product)
+    useEffect(() => {
+        setData(product)
+        setSearch(product)
+    }, [product], [product]);
 
     const columns = [
         {
-            name: "Title",
-            title: "title",
-            selector: row => row.title
-        }, {
+            name: "No.",
+            selector: (row) => row.id
+        },
+        {
+            name: "Products",
+            selector: (row) => row.title,
+        },
+        {
             name: "Price",
-            price: "price",
-            selector: row => row.price
-        }, {
-            name: "Description",
-            description: "description",
-            selector: row => row.description
+            selector: (row) => row.price,
+            sortable: true,
+        },
+        {
+            name: "rate",
+            selector: (row) => row.rating.rate,
         },
         {
             name: "Images",
-            images: "images",
-            selector: row => <img src={row.images} width={50} height={50} />
-        },
-        {
-            name: "Action",
             cell: (row) => (
-                <>
-                    <button
-                        onClick={() =>
-                            alert("Sorry, edit function is not implemented yet!")
-                        }
-                        className="btn btn-primary me-2"
-                    >
-                        Edit
-                    </button>
-                    <button
-                        onClick={() =>
-                            alert("Sorry, delete function is not implemented yet!")
-                        }
-                        className="btn btn-danger"
-                    >
-                        Delete
-                    </button>
-                </>
+                <img
+                    style={{ width: "100px", padding: "3px 0 3px 0" }}
+                    src={row.image} alt={row.title}
+                />
             ),
         },
+        {
+            name: "Actions", 
+            cell: (row) => (
+                <>
+                    <a className='btn btn-success mx-1' href="">Like</a>
+                    <a className='btn btn-danger mx-1' href="">Delete</a>
+                </>
+            )
+        }
     ]
-
-    function handleFilter(event) {
-        const newData = product.filter((row) => {
-            return row.title.toLowerCase().includes(event.target.value.toLowerCase());
-        });
-        setproduct(newData);
+    function handleSearching(event) {
+        const data_searching = data.filter(data => data.title.toLowerCase().includes(event.target.value.toLowerCase())); 
+        setSearch(data_searching); 
     }
-
     return (
-        <Layout products>
+        <Layout products >
 
             <div class="container">
-                <DataTable columns={columns} data={product}
-                    fixedHeader
-                    highlightOnHover
-                    title="Product listing"
-                    pagination
-                    actions={
-                        <div className="text-end">
-                            <input
-                                type="text"
-                                className="rounded border-1"
-                                style={{
-                                    padding: "10px",
-                                }}
-                                placeholder="Search products"
-                                onChange={handleFilter}
-                            ></input>
-                        </div>
-                    }
-                />
+
+                <div class="row">
+                    <DataTable
+                        columns={columns} data={search} fixedHeader pagination title="Products"
+                        actions={
+                            <div className="text-end">
+                                <input
+                                    type="text"
+                                    className="rounded border-1"
+                                    style={{
+                                        padding: "3px 10px",
+                                        width: "300px",
+                                        fontSize: "15px",
+                                    }}
+                                    placeholder="Search"
+                                    onChange={handleSearching}
+                                ></input>
+                            </div>
+                        }
+                    >
+                    </DataTable>
+                </div>
+
             </div>
 
         </Layout>
     )
+} export default Products;
+
+
+export async function getServerSideProps() {
+    const res = await fetch('https://fakestoreapi.com/products')
+    const fetched = await res.json();
+
+    return {
+        props: {
+            product: fetched,
+        }
+    }
 }
-
-
